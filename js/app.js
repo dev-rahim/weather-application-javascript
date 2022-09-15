e0f2620e2cf9436f67eec6b58aa369696900fdd5
dev-rahim <rahimuddin5915@gmail.com>
2022-09-15 17:48:24 +0600

    working with weather api

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const loadData = async () => {
+
+    const searchFild = document.getElementById('search');
+    const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=9346bda862bd4deea51181702222605&q=${searchFild.value}&aqi=no`);
+    // const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchFild.value}&appid=c2756ab52ce9283c29c4a3cd34eba79b`);
+    const data = await res.json();
+    console.log(data);
+
+
+    document.getElementById('weather-status-container').innerHTML = `
+            <img src="${data.current.condition.icon}" alt="">
+            <h1>${data.location.name}</h1>
+            <h3>${data.location.country}</h3>
+            <h3><span>${data.current.temp_c}</span>&deg;C</h3>
+            <h1 class="lead">${data.current.condition.text}</h1>
+    `;
+}
